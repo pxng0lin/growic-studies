c69a0a80b581298047b63b2f2a8c21fc9f576fc6
pxng0lin <60779090+pxng0lin@users.noreply.github.com>
2022-12-27 23:13:34 +0000

    Update mappings-task.sol

--- tasks/mappings-task.sol
+++ tasks/mappings-task.sol
@@ -15,8 +15,24 @@ contract GrowicBank {
         owner = msg.sender;
     }
 
+    // created an internal function so that the balance can be updated rather than overidden
+    function updateBalance(address _addr, uint256 _toAdd) internal {
+        require(_toAdd > 0, "Please provide an amount more than 0"); // check that the deposit amount is more than 0, otherwise revert and show msg
+        balance[_addr] += _toAdd;
+    }
+
     function deposit(address _customer, uint256 _deposit) external {
-        balance[_customer] = _deposit;
+        /** 
+         * Added an if statement so we can add to the balance, rather than just override the current one
+         * source: https://www.tutorialspoint.com/solidity/solidity_if_else_statement.htm
+        */
+
+        if(balance[_customer] > 0) {
+            updateBalance(_customer, _deposit);
+        } else {
+            balance[_customer] = _deposit;
+        }
+        
     }
 
     function checkBalance() public view returns(uint256) {
